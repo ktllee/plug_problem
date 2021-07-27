065a53e888032a8db8edb4f4a5b64b3a9fc7f5ed
Ethan Bolker <eb@cs.umb.edu>
2021-07-27 12:51:03 -0400

    added functionality

--- growth.py
+++ growth.py
@@ -1,15 +1,15 @@
-
 """
 code to calculate growth rate for plug puzzles using
 arbitrary numbers of plugs R_k = 11....1
 
-last modified: 07/17/21
+last modified: 07/27/21
 
 @author: Ethan Bolker
 """
 import sys
 import math
 from convolve import p2t
+from sympy import *
 
 def mygcd(mylist):
     if len(mylist) == 1:
@@ -17,6 +17,21 @@ def mygcd(mylist):
     else:
         return math.gcd( mylist[0],mygcd(mylist[1:]))
 
+def build_recursion_polynomial(bits):
+    ''' from input bit string 1011 build recursion polynomial
+    x**4 - x**3 -x**1 -1
+    '''
+    n = len(bits)
+    poly = 'x**'
+    poly += str(n)
+    for j in range(n)[:-1]:
+        b = int(bits[j])
+        if b > 0:
+            poly += ('-x**') + str(n-j-1)
+    poly += '-1'
+    return poly
+
+    
 def growthrate(d):
     ''' Calculate the growth rate for total solutions
     for the puzzle problem R(d). Here d is the  
@@ -42,20 +57,29 @@ def growthrate(d):
     rate = totals[1000]/totals[999]
     return(rate)
 
+def growthratecsv(N):
+    print("d,  growth rate, poly, factored, primitive")
+    for d in range(1, 1+2**N,2):
+        todo = [bin(d)[2:], bin(2*d)[2:][::-1], bin(4*d)[2:][::-1],
+                bin(8*d)[2:][::-1], bin(16*d)[2:][::-1] 
+        ]
+        for bits in todo:
+            poly = build_recursion_polynomial(bits)
+            fpoly = factor(poly)
+            print(f"{bits}, {growthrate(bits)}, {poly}, {fpoly}")
+    return
     
-#  Execute for testing
+#  Execute 
 if __name__ == "__main__":
 
     if (len(sys.argv) == 1):
-        print(f"usage: python {sys.argv[0]} N (for rates up to d = 2^N - 1)")
+        print(f"usage: python {sys.argv[0]} bits1 bits2 ...")
     else:
-        N = int(sys.argv[1])
-        print(N)
-        print("d,  growth rate")
-        for d in range(1, 1+2**N,2):
-            print(f"{bin(d)[2:]}, {growthrate(d)}")
-            print(f"{bin(2*d)[2:][::-1]}, {growthrate(bin(2*d)[2:][::-1])}")
-            print(f"{bin(4*d)[2:][::-1]}, {growthrate(bin(4*d)[2:][::-1])}")
+        for bits in sys.argv[1:]:
+            poly = build_recursion_polynomial(bits)
+            fpoly = factor(poly)
+            print(f"{bits}, {growthrate(bits)}, {poly}, {fpoly}")
+
+        #     growthratecsv(7)
 
 
-              

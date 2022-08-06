69c21d3783bd7788784976eaa31f919897def058
Katie <ktllee@bu.edu>
2022-08-06 00:50:29 -0400

    update: init for rodset

--- cuisenaire/rods.py
+++ cuisenaire/rods.py
@@ -19,16 +19,35 @@ description:
     attributes:
         basic - list of rod lengths
         string - string listing rod lengths (e.g. 011 = [2,3])
-    
+        coeffs - list of coeffs for the rod polynomial
+        roots - all the roots (absolute value, rounded to 10 places)
+        growth - max root
+        size - degree of polynomial
+        fullpoly - the full polynomial, written out
+        facpoly - the full polynomial factored
+        minimal - the minimal polynomial
+        shift - the shift polynomial (fully expanded)
+                
     methods:
         init, repr, eq
         copy - deep copy
         spotcon (helper) - switch between basic and string representations
+        coefcon (helper) - switch between polynomial string and coef list
         
     dependencies:
-        factor() from sympy, as factor
+        factor() from sympy
+        polyroots() from numpy.polynomial.polynomial
         
 """
+# dependencies
+# packages
+import numpy as np
+
+# functions
+from sympy import factor_list
+from numpy.polynomial.polynomial import polyroots
+from numpy.polynomial.polynomial import polydiv
+
 
 class Rodset:
     """ class for set of cuisenaire rods
@@ -39,11 +58,8 @@ class Rodset:
         e.g. the padovans would be [2,3] or '011'.  note [2,2] is '02'
     """
     
-    # dependencies
-    from sympy import factor as factor
-    
+    ########## helpers
     # helper: string to basic converter
-    # needed only for backwards compatibiity
     @staticmethod
     def spotcon(rep):
         ''' changes a string representaion of a rodset to a list
@@ -66,12 +82,103 @@ class Rodset:
         return converted
     
     
+    # helper: string to basic converter
+    @staticmethod
+    def coefcon(poly):
+        ''' changes a string representaion of a polynomial to a list of coefs
+            or vice versa
+            
+            note:
+                - assumes rep is already correctly one or the other
+                - poly strings must be fully expanded
+        '''
+        
+        # string to list
+        if isinstance(poly, str):
+            # find degrees and coefficients
+            power = {}
+            sign = 1
+            for t in poly.split():
+                if t == '-':
+                    sign = -1
+                elif t == '+':
+                    sign = 1
+                else:
+                    term = t.split('*')
+                    deg, coef = 1, 1
+                    if '' in term:
+                        deg = int(term[-1])
+                    elif 'x' not in term:
+                        deg = 0
+                    if term[0] != 'x':
+                        coef = int(term[0])
+                    power[deg] = coef * sign
+            # convert to coefficient list
+            converted = [0] * (max(power.keys()) + 1)
+            for i in range(len(converted)):
+                if i in power.keys():
+                    converted[i] = power[i]
+            
+        # list to string
+        elif isinstance(poly, list):
+            converted = ''
+            top = len(poly) - 1
+            # iterate through coefficients
+            for deg, coef in enumerate(poly):
+                if top == 0:
+                    converted = str(coef)
+                    break
+                term = ''
+                # handle coefficient
+                if coef == 1:
+                    term += ' + '
+                elif coef == -1:
+                    term += ' - '
+                elif coef > 0:
+                    term += f" + {coef}*"
+                elif coef < 0:
+                    term += f" - {abs(coef)}*"
+                # handle degree
+                if coef != 0:
+                    if deg == 0:
+                        if abs(coef) == 1:
+                            term += '1'
+                        else:
+                            term = term[:-1]
+                    elif deg == 1:
+                        term += 'x'
+                    elif deg < top:
+                        term += f"x**{deg}"
+                    else:
+                        if coef == 1:
+                            term = f"x**{deg}"
+                        elif coef == -1:
+                            term = f"-x**{deg}"
+                        else:
+                            term = f"{coef}*x**{deg}"
+                # add to converted
+                converted = term + converted
+        
+        return converted
+    
+    
+    ########## class methods
+    # initialize
     def __init__(self, represent):
         ''' intitalizes from a descriptive attribute (represent),
-            either list of lengths or bit string
+            either list of lengths (basic) or bit string (string)
                 
             also initializes:
+                coeffs
+                roots
                 growth
+                size
+                fullpoly
+                facpoly
+                minimal
+                shift
+                
+            note: ONLY works for rod polys
         '''
         
         # find which argument was given and fill others
@@ -103,9 +210,44 @@ class Rodset:
         self.basic = basic
         
         # polynomials
+        # coefficients
+        coeffs = [-int(x) for x in string][::-1] + [1]
+        self.coeffs = coeffs
+        
+        # growth rate
+        roots = np.round(np.abs(polyroots(coeffs)), 10)
+        growth = max(roots)
+        size = len(coeffs) - 1
+        self.roots = roots
+        self.growth = growth
+        self.size = size
+        
+        # full polynomial
+        fullpoly = self.coefcon(coeffs)
+        self.fullpoly = fullpoly
+        
+        # factored
+        facpoly = [(str(x[0]), x[1]) for x in factor_list(fullpoly)[1]]
+        self.facpoly = facpoly
+        
+        # minimal and shift
+        minimal = []
+        for fac, deg in facpoly:
+            if max(np.round(np.abs(polyroots(self.coefcon(fac))),
+                            10)) == growth:
+                minimal.append(fac)
+        if len(minimal) == 1:
+            minimal = minimal[0]
+        else:
+            raise ValueError('multiple or no minimal polynomials found')
+        shiftcoefs = [int(x) if x.is_integer else x for x in \
+                      polydiv(coeffs, self.coefcon(minimal))[0].tolist()]
+        shift = self.coefcon(shiftcoefs)
+        self.minimal = minimal
+        self.shift = shift
         
         
-        
+    # represent
     def __repr__(self):
         ''' basic (list) representation
         '''
@@ -113,6 +255,7 @@ class Rodset:
         return self.basic
     
     
+    # equal
     def __eq__(self, other):
         ''' rodsets are the same if they have the same string
             if other is not Rodset, return False.
@@ -127,6 +270,7 @@ class Rodset:
             return False
         
     
+    # (deep) copy
     def copy(self):
         ''' takes: self
             returns: a deep copy of self
